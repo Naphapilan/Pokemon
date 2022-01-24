60d07ea308c23c79474103abfd7077c8a420db2b
Naphapilan <96216485+Naphapilan@users.noreply.github.com>
2022-01-24 15:12:59 +0700

    upload

---
+++ Pokemon Battle/Pokemon Battle/Pikachu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Battle
+{
+    internal class Pikachu : Pokemon
+    {
+        public Pikachu ()
+        { 
+            name = 'Pikachu';
+            height = 1.04;
+            weight = 13.2;
+
+            gender = Gender.male;
+            category = Category.mouse;
+            abilities = Abilities.static_;
+            type = Type.eletric;
+            wealnesses =
+
+        }
+    }
+}
